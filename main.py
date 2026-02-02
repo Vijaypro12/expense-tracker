9677e735eb420433cb2c9bda122b477b801cd103
VIJAY <144886505+Vijaypro12@users.noreply.github.com>
2026-02-02 22:36:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import csv
+from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    COLUMNS=["date", "amount", "category", "description"]
+    FORMAT="%d-%m-%Y"
+
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df=pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index=False)
+    
+    @classmethod
+    def add_entry(cls, date, amount, category, description):
+
+        new_entry={
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }    
+        
+        with open(cls.CSV_FILE, "a", newline='')as csvfile:
+            writer=csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)
+            writer.writerow(new_entry)
+        print("Entry added successfully!")    
+ 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df=pd.read_csv(cls.CSV_FILE)
+        df["date"]=pd.to_datetime(df["date"], format=cls.FORMAT) 
+        start_date=datetime.strptime(start_date, cls.FORMAT)
+        end_date=datetime.strptime(end_date, cls.FORMAT)
+
+        mask=(df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df=df.loc(mask)
+
+        if filtered_df.empty:
+            print("No transcations found in the given date")
+        else:
+            print(f"Transcations from {start_date.strftime(cls.FORMAT)} to {end_date.strftime(cls.FORMAT)}")
+             
+
+   
+def add():
+    CSV.initialize_csv()
+    date=get_date("Enter the date of the transcation (dd-mm-yyyy) or enter todays date : ", allow_default=True)
+    amount=get_amount()
+    category=get_category()
+    description=get_description()
+    CSV.add_entry(date, amount, category, description)
+
+    
